9ad6c9c4531c46c0d216690b2041c8461a88b747
0wriken <wriken@163.com>
2019-12-23 00:30:35 +0800

    C#

--- C#/note.cs
+++ C#/note.cs
@@ -65,10 +65,11 @@ namespace idexTest
 	}
 class student
 {
-	private  Dictionary<string ,int>sorce=new Dictionary<string ,int>(); //调用库泛型编程为string类型与下标值
+	private Dictionary<string ,int>sorce=new Dictionary<string ,int>(); //调用库泛型编程为string类型与下标值
 	public int? this[string subject]
 	{
-		get 
+
+		get
 		{
 			if(this.sorce.ContainKey[subject])  
 			{
@@ -152,7 +153,7 @@ class student
 	static void Main(string[] args)
 	{
 		student stu=null;
-		if(Create(stu))
+		if(Create(stu))  //参数？？？
 		{
 			console.WriteLine(stu.name,stu.age);
 		}
@@ -163,15 +164,15 @@ class student
 		public int age{get;set;};
 		public string name{get;set;};
 	}
-	static bool Create(string name,int age,out student result)
+	static bool Create(out student result)
 	{
 		result=null;
-		if(string.isEmpttyorNull(name))
+		if(student.name.isEmptyorNull(name))
 		{
 
 			return false;
 		}
-		if(age>20||age<80)
+		if(student.age>20||student.age<80)
 		{
 			return false;
 		}
@@ -240,19 +241,23 @@ delegate 是函数指针的升级版
 	{
 		int x,y,z;
 		Calculator cal= new Calculator();
-		Action action=new Action(cal.Add);//委托函数名代表地址 
+		Action action=new Action(cal.pri);//委托函数名代表地址 
 		//使用函数
-		z=action();
-		z=action.lnvoke();
+		action();
+		action.lnvoke();
 		//Func
 		Func <int,int ,int> func1=new Func<int ,int,int>(cal.Add);
 		Func <int,int ,int> func2=new Func<int ,int,int>(cal.Sub);
-		x=func1.lnvoke();
-		x=func2.lnvoke();
+		x=func1.lnvoke(1,2);
+		y=func2.lnvoke(3,2);
 	}
 
 	class Calculator
 	{
+		public void pri()
+		{
+			console.WriteLine("hello");
+		}
 		public int Add(int a, intn b)
 		{
 			int result =a+b;
@@ -264,6 +269,141 @@ delegate 是函数指针的升级版
 			return result;
 		}
 	}
+自定义委托：
+	namespace DelegateExamle
+	{
+		//注意类型兼容
+		public delegate double Calu(double x,double y);
+		class Calculator 
+		{
+			static void Main(String[] args)
+			{
+				Calculator cal=new Calculator();
+				Calu func1=new Calu(cal.Add);
+				Calu func2=new Calu(cal.Sub);
+				console.WriteLine(func1.lnvoke(1,2),func2.lnvoke(3,2));			
+			}
+		}
+	}
+	class Calculator
+	{
+		public void pri()
+		{
+			console.WriteLine("hello");
+		}
+		public int Add(int a, intn b)
+		{
+			int result =a+b;
+			return result;
+		}
+		public int Sub(int a, intn b)
+		{
+			int result =a-b;
+			return result;
+		}
+	}
+委托的使用：模板方法，回调方法
+	模板方法：	
+	static void Main(string[] args)
+	{
+		ProFactor factory=new ProFactor();
+		WrapFactry warp=new WrapFactry();
+		Logger log=new Logger();
+	/*	warp.WrapProduct(factory.MakePizza);
+		warp.WrapProduct(factory.MakeCar);	*/
+		Func<product> func1=new Func<product>(factory.MakePizza);
+		Func<product> func2=new Func<product>(factory.MakeCar);
+		Action<product>func3=new Action<product>(log);
+		Box box1 = new Box();
+		Box box2= new Box();
+		box1 = warp.WrapProduct(func1,func3);
+		box2 = warp.WrapProduct(func2,func3);
+		console.WriteLine(box1.product.Name);
+		console.WriteLine(box2.product.Name);	
+	}
+	class product
+	{
+		public string Name{get;set;}
+		public int price{get;set;}
+	}
+	class Box
+	{
+		public product{get;set;}
+	}
+	class WrapFactry
+	{
+		//函数内部以委托为参数调用。
+		public Box WrapProduct(Func<product> getpro,Action<product> log)
+		{
+			Box box =new Box();
+			//这里兼容各种同类型的委托，实现多种方法
+			box.product=getpro.lnvoke();
+			//使用回调函数
+			if(box.product>=52)
+			{
+				log.Log(box.product); //以添加函数参数做调用
+			}
+			return box;
+		}
+	}
+	class ProFactor
+	{
+		public product MakePizza()
+		{
+			product Pizza=new product();
+			Pizza.Name="Pizza";
+			Pizza.price=60;
+			return Pizza;
+		}
+		public product MakeCar()
+		{
+			product Car=new product();
+			Car.Name="car";
+			Car.price=50;
+			return Car;
+		}
+	}
+
+	//记录程序的运行状态
+	class Logger
+	{
+		public void Log(product pro)
+		{
+			console.WriteLine(product.Name,DataTime.UtcNow.product.price);
+		}
+	}
+缺点：这是一种方法级别的紧耦合，可能突破进程内存，注意使用
+	可读性下降、debug难度下降
+异步调用：
+	隐式
+	BeginInvoke(null,null);
+尽量用接口取代委托
+6.事件与委托的关系
+事件拥有者+事件成员+事件响应者+事件处理器+事件订阅
+ 	using System.Timer
+ 	class Peogram
+ 	{
+ 		static void Main(string[] args)
+ 		{
+ 			Timer time =new Timer();
+ 			time.Inteval=1000;
+ 			Boy boy=new Boy();
+ 			time.Elapsed+=boy.Action;//订阅事件
+ 			time.start();
+ 			console.WriteLine(" ");
+ 		}
+ 	}
+ 	class Boy
+ 	{ 		//事件处理器
+ 		internal void Action(object sender,ElapsedEnentArgs e)
+ 		{
+ 			console.WriteLine("hello");
+
+ 		}
+ 	}
+
+
+
 
 
 
